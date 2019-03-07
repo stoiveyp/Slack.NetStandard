d118099f2b4894827eb22da124c5fd183ff7cf92
Steven Pears <steven.pears@live.co.uk>
2019-03-07 18:19:28 +0000

    initial slack command parsing

---
+++ Slack.NetStandard.Tests/SlashCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Slack.NetStandard.Tests
+{
+    public class SlashCommandTests
+    {
+        private const string payload = @"token=gIkuvaNzQIHg97ATvDxqgjtO
+&team_id=T0001
+&team_domain=example
+&enterprise_id=E0001
+&enterprise_name=Globular%20Construct%20Inc
+&channel_id=C2147483705
+&channel_name=test
+&user_id=U2147483697
+&user_name=Steve
+&command=/weather
+&text=94070
+&response_url=https://hooks.slack.com/commands/1234/5678
+&trigger_id=13345224609.738474920.8088930838d88f008e0";
+
+        [Fact]
+        public void SlashCommandParsesCorrectly()
+        {
+            var command = new SlashCommand(payload + "&test=1");
+            Assert.True(command.Payload.ContainsKey("test"));
+            Assert.Equal("1", command.Payload["test"]);
+        }
+    }
+}
---
+++ Slack.NetStandard/SlashCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+
+namespace Slack.NetStandard
+{
+    public class SlashCommand
+    {
+        public SlashCommand(string payloadText) : this(Parse(payloadText))
+        {
+
+        }
+
+        public SlashCommand(Dictionary<string, string> payload)
+        {
+            Payload = payload;
+        }
+
+        private static Dictionary<string, string> Parse(string data)
+        {
+            var coll = HttpUtility.ParseQueryString(data);
+            return coll.AllKeys.ToDictionary(k => k, k => coll[k]);
+        }
+
+
+        public Dictionary<string, string> Payload { get; }
+    }
+}
