f0d4644f91323d0cb542e801be368605ee71a304
Steven Pears <steven.pears@live.co.uk>
2019-12-17 11:54:20 +0000

    Update postMessageRequest

--- Slack.NetStandard/WebApi/Chat/PostMessageRequest.cs
+++ Slack.NetStandard/WebApi/Chat/PostMessageRequest.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 using Slack.NetStandard.Messages;
 using Slack.NetStandard.Messages.Blocks;
 
 namespace Slack.NetStandard.WebApi.Chat
 {
     public class PostMessageRequest : Message {
+        [JsonProperty("channel")]
+        public string Channel { get; set; }
+
+        [JsonProperty("as_user")]
+        public bool AsUser { get; set; }
     }
 }
