3d1b87c274c4db396e7ca40a9289b2ac433076bc
Steven Pears <steven.pears@live.co.uk>
2019-12-17 23:59:46 +0000

    Add all postmessagerequest properties

---
+++ Slack.NetStandard/WebApi/Chat/MessageParsing.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Slack.NetStandard.WebApi.Chat
+{
+    public enum MessageParsing
+    {
+        [EnumMember(Value="none")]
+        None,
+        [EnumMember(Value="full")]
+        Full
+    }
+}
