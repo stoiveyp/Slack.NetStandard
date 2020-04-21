b6b94d12411aa28c9f8fc8cd9327f885c8554868
Steven Pears <steven.pears@live.co.uk>
2020-04-21 21:19:27 +0100

    ViewUpdate ViewPublish ViewPush

---
+++ Slack.NetStandard/WebApi/View/UpdateViewRequest.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Slack.NetStandard.WebApi.View
+{
+    internal class UpdateViewRequest
+    {
+        [JsonProperty("view")]
+        public Objects.View View { get; set; }
+
+        [JsonProperty("external_id",NullValueHandling = NullValueHandling.Ignore)]
+        public string ExternalId { get; set; }
+
+        [JsonProperty("view_id",NullValueHandling = NullValueHandling.Ignore)]
+        public string ViewId { get; set; }
+
+        [JsonProperty("hash",NullValueHandling = NullValueHandling.Ignore)]
+        public string Hash { get; set; }
+    }
+}
--- Slack.NetStandard/WebApi/ViewApi.cs
+++ Slack.NetStandard/WebApi/ViewApi.cs
@@ -12,9 +12,29 @@ namespace Slack.NetStandard.WebApi
             _client = client;
         }
 
-        public Task<ViewResponse> Publish(string userId, Objects.View view, string hash = null)
+        public Task<ViewResponse> Publish(string user, Objects.View view, string hash = null)
         {
-            return _client.MakeJsonCall<PublishViewRequest,ViewResponse>("views.publish", new PublishViewRequest {User = userId, View = view, Hash = hash});
+            return _client.MakeJsonCall<PublishViewRequest,ViewResponse>("views.publish", new PublishViewRequest {User = user, View = view, Hash = hash});
+        }
+
+        public Task<ViewResponse> Open(string trigger, Objects.View view)
+        {
+            return _client.MakeJsonCall<TriggerViewRequest, ViewResponse>("views.open", new TriggerViewRequest { Trigger = trigger, View = view });
+        }
+
+        public Task<ViewResponse> Push(string trigger, Objects.View view)
+        {
+            return _client.MakeJsonCall<TriggerViewRequest, ViewResponse>("views.push", new TriggerViewRequest { Trigger = trigger, View = view });
+        }
+
+        public Task<ViewResponse> UpdateByExternalId(string externalId, Objects.View view, string hash = null)
+        {
+            return _client.MakeJsonCall<UpdateViewRequest, ViewResponse>("views.update", new UpdateViewRequest { ExternalId = externalId, View = view, Hash = hash });
+        }
+
+        public Task<ViewResponse> UpdateByViewId(string viewId, Objects.View view, string hash = null)
+        {
+            return _client.MakeJsonCall<UpdateViewRequest, ViewResponse>("views.update", new UpdateViewRequest { ViewId = viewId, View = view, Hash = hash });
         }
     }
 }
