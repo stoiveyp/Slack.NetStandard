68a55e7567c977402534cb0bd33e8fcac9c192cf
Steven Pears <steven.pears@live.co.uk>
2020-04-21 18:49:16 +0100

    Seperate usergroup users api

--- Slack.NetStandard/WebApi/UsergroupsApi.cs
+++ Slack.NetStandard/WebApi/UsergroupsApi.cs
@@ -78,42 +78,4 @@ namespace Slack.NetStandard.WebApi
             return _client.MakeUrlEncodedCall<UsergroupListResponse>("usergroups.list", dict);
         }
     }
-
-    internal class UsergroupsUserApi : IUsergroupUserApi
-    {
-        private readonly IWebApiClient _client;
-
-        public UsergroupsUserApi(IWebApiClient client)
-        {
-            _client = client;
-        }
-
-        public Task<UsergroupUserListResponse> List(string usergroup, bool? includeDisabled = null)
-        {
-            var dict = new Dictionary<string, string>{{nameof(usergroup),usergroup}};
-
-            if (includeDisabled.HasValue)
-            {
-                dict.Add("include_disabled", includeDisabled.Value.ToString().ToLower());
-            }
-
-            return _client.MakeUrlEncodedCall<UsergroupUserListResponse>("usergroups.users.list", dict);
-        }
-
-        public Task<UsergroupResponse> Update(string usergroup, string[] users, bool? includeCount = null)
-        {
-            var dict = new Dictionary<string, string>
-            {
-                {nameof(usergroup), usergroup},
-                {nameof(users),string.Join(",",users)}
-            };
-
-            if (includeCount.HasValue)
-            {
-                dict.Add("include_count", includeCount.Value.ToString().ToLower());
-            }
-
-            return _client.MakeUrlEncodedCall<UsergroupResponse>("usergroups.users.update", dict);
-        }
-    }
 }
---
+++ Slack.NetStandard/WebApi/UsergroupsUserApi.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Slack.NetStandard.WebApi.Usergroups;
+
+namespace Slack.NetStandard.WebApi
+{
+    internal class UsergroupsUserApi : IUsergroupUserApi
+    {
+        private readonly IWebApiClient _client;
+
+        public UsergroupsUserApi(IWebApiClient client)
+        {
+            _client = client;
+        }
+
+        public Task<UsergroupUserListResponse> List(string usergroup, bool? includeDisabled = null)
+        {
+            var dict = new Dictionary<string, string>{{nameof(usergroup),usergroup}};
+
+            if (includeDisabled.HasValue)
+            {
+                dict.Add("include_disabled", includeDisabled.Value.ToString().ToLower());
+            }
+
+            return _client.MakeUrlEncodedCall<UsergroupUserListResponse>("usergroups.users.list", dict);
+        }
+
+        public Task<UsergroupResponse> Update(string usergroup, string[] users, bool? includeCount = null)
+        {
+            var dict = new Dictionary<string, string>
+            {
+                {nameof(usergroup), usergroup},
+                {nameof(users),string.Join(",",users)}
+            };
+
+            if (includeCount.HasValue)
+            {
+                dict.Add("include_count", includeCount.Value.ToString().ToLower());
+            }
+
+            return _client.MakeUrlEncodedCall<UsergroupResponse>("usergroups.users.update", dict);
+        }
+    }
+}
