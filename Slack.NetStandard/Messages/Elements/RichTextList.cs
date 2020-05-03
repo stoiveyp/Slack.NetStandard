568b011d6641219c44472f9052d62cd3bbfe18d6
Sho Sato <rizeupbass@gmail.com>
2020-05-03 12:00:46 +0900

    Support rich_text elements

---
+++ Slack.NetStandard/Messages/Elements/RichTextList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Slack.NetStandard.Messages.Elements.RichText;
+
+namespace Slack.NetStandard.Messages.Elements
+{
+    public class RichTextList : IMessageElement
+    {
+        public const string ElementType = "rich_text_list";
+        public string Type => ElementType;
+
+        [JsonProperty("elements", NullValueHandling = NullValueHandling.Ignore)]
+        public IList<RichTextElement> Elements { get; set; }
+
+        [JsonProperty("style", NullValueHandling = NullValueHandling.Ignore)]
+        public string Style { get; set; }
+
+        [JsonProperty("indent", NullValueHandling = NullValueHandling.Ignore)]
+        public int? Indent { get; set; }
+    }
+}
---
+++ Slack.NetStandard/Messages/Elements/RichTextQuote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Slack.NetStandard.Messages.Elements.RichText;
+
+namespace Slack.NetStandard.Messages.Elements
+{
+    public class RichTextQuote : IMessageElement
+    {
+        public const string ElementType = "rich_text_quote";
+        public string Type => ElementType;
+
+        [JsonProperty("elements", NullValueHandling = NullValueHandling.Ignore)]
+        public IList<RichTextElement> Elements { get; set; }
+    }
+}
