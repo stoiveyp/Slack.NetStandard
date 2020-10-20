ef9369f01e014f8bae0e54a7867e7e8aa623ce95
Steven Pears <steven.pears@live.co.uk>
2020-10-20 11:29:07 +0100

    Add authorizations

---
+++ Slack.NetStandard/JsonConverters/AuthorizationConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Slack.NetStandard.Objects;
+
+namespace Slack.NetStandard.JsonConverters
+{
+    public class AuthorizationConverter:SingleOrArrayConverter<Authorization>
+    {
+        protected override JsonToken SingleToken => JsonToken.StartObject;
+        protected override void ReadSingle(JsonReader reader, JsonSerializer serializer, List<Authorization> list)
+        {
+            var item = serializer.Deserialize<Authorization>(reader);
+            list.Add(item);
+        }
+    }
+}
---
+++ Slack.NetStandard/Objects/Authorization.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Slack.NetStandard.Objects
+{
+    public class Authorization
+    {
+        [JsonProperty("enterprise_id",NullValueHandling = NullValueHandling.Ignore)]
+        public string EnterpriseId { get; set; }
+
+        [JsonProperty("team_id",NullValueHandling = NullValueHandling.Ignore)]
+        public string TeamId { get; set; }
+
+        [JsonProperty("user_id",NullValueHandling = NullValueHandling.Ignore)]
+        public string UserId { get; set; }
+
+        [JsonProperty("is_bot",NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IsBot { get; set; }
+    }
+}
