8a5bbdb5a2a7c8f55e7ec515ab252460b0cfe5dc
Steven Pears <steven.pears@live.co.uk>
2021-04-07 11:30:11 +0100

    Add view state converter

---
+++ Slack.NetStandard/JsonConverters/ViewStateConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Slack.NetStandard.Objects;
+
+namespace Slack.NetStandard.JsonConverters
+{
+    public class ViewStateConverter:JsonConverter<Dictionary<string, Dictionary<string, ElementValue>>>
+    {
+        public override bool CanWrite => false;
+
+        public override void WriteJson(JsonWriter writer, Dictionary<string, Dictionary<string, ElementValue>> value, JsonSerializer serializer)
+        {
+            
+        }
+
+        public override Dictionary<string, Dictionary<string, ElementValue>> ReadJson(JsonReader reader, Type objectType, Dictionary<string, Dictionary<string, ElementValue>> existingValue, bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.StartArray)
+            {
+                reader.Read();
+                reader.Read();
+                return null;
+            }
+
+            return serializer.Deserialize<Dictionary<string, Dictionary<string, ElementValue>>>(reader);
+        }
+    }
+}
--- Slack.NetStandard/Objects/ViewState.cs
+++ Slack.NetStandard/Objects/ViewState.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections.Generic;
 using Newtonsoft.Json;
+using Slack.NetStandard.JsonConverters;
 
 namespace Slack.NetStandard.Objects
 {
     public class ViewState
     {
         [JsonProperty("values",NullValueHandling = NullValueHandling.Ignore)]
+        [JsonConverter(typeof(ViewStateConverter))]
         public Dictionary<string, Dictionary<string,ElementValue>> Values { get; set; }
     }
 }
