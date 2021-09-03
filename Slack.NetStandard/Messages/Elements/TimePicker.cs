314b226836c210c8a20400953f87fa5a24375c1d
Steven Pears <steven.pears@live.co.uk>
2021-09-03 19:51:39 +0100

    Add TimePicker

---
+++ Slack.NetStandard/Messages/Elements/TimePicker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Slack.NetStandard.Messages.Elements
+{
+    public class TimePicker : IMessageElement
+    {
+        public TimePicker() { }
+
+        public TimePicker(string actionId)
+        {
+            ActionId = actionId;
+        }
+
+        public string Type => nameof(TimePicker).ToLower();
+
+        [JsonProperty("placeholder")]
+        public PlainText Placeholder { get; set; }
+
+        [JsonProperty("action_id")]
+        public string ActionId { get; set; }
+
+        [JsonProperty("initial_time", NullValueHandling = NullValueHandling.Ignore)]
+        public string InitialTime { get; set; }
+
+        [JsonProperty("confirm", NullValueHandling = NullValueHandling.Ignore)]
+        public Confirmation Confirm { get; set; }
+    }
+}
