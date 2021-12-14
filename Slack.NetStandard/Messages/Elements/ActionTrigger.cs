01c4f373f30ba3f2fdff4568f9f7118c2ebe65e7
Dustin L. Howett <dustin@howett.net>
2021-12-13 19:35:32 -0600

    Add dispatch_action_config (et al) to plain_text_input
    This commit introduces support for configuring the
    [`dispatch_action_config`] and its `trigger_actions_on` member to input
    block elements that contain a [`plain_text_input`].
    
    [`dispatch_action_config`]: https://api.slack.com/reference/block-kit/composition-objects#dispatch_action_config
    [`plain_text_input`]: https://api.slack.com/reference/block-kit/block-elements#input

---
+++ Slack.NetStandard/Messages/Elements/ActionTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Slack.NetStandard.Messages.Elements
+{
+    public enum ActionTrigger
+    {
+        [EnumMember(Value = "on_enter_pressed")]
+        OnEnterPressed,
+        [EnumMember(Value = "on_character_entered")]
+        OnCharacterEntered
+    }
+}
---
+++ Slack.NetStandard/Messages/Elements/DispatchActionConfig.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Slack.NetStandard.Messages.Elements
+{
+    public class DispatchActionConfig
+    {
+        public DispatchActionConfig(ActionTrigger[] triggers)
+        {
+            TriggerActionsOn = triggers;
+        }
+
+        [JsonProperty("trigger_actions_on", NullValueHandling = NullValueHandling.Ignore, ItemConverterType = typeof(StringEnumConverter))]
+        public ActionTrigger[] TriggerActionsOn { get; set; }
+    }
+}
