76b17a13ef85565ae68f7d3d44114cdde96dd895
Steven Pears <steven.pears@live.co.uk>
2022-01-12 08:24:20 +0000

    Add support for admin.apps.requests.cancel

--- Slack.NetStandard/WebApi/Admin/TeamFilter.cs
+++ Slack.NetStandard/WebApi/Admin/TeamFilter.cs
@@ -1,10 +1,7 @@
-﻿using Newtonsoft.Json;
-
-namespace Slack.NetStandard.WebApi.Admin
+﻿namespace Slack.NetStandard.WebApi.Admin
 {
     public class TeamFilter : TeamRequestFilter
     {
-        [JsonProperty("enterprise_id",NullValueHandling = NullValueHandling.Ignore)]
-        public string Enterprise { get; set; }
+        
     }
 }
