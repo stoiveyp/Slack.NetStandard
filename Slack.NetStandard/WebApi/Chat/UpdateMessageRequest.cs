e7bee4e712e378f11b89ad16406c9b2cc98cff86
Steven Pears <steven.pears@live.co.uk>
2022-06-03 22:25:35 +0100

    Chat.update added file_ids and attachments

--- Slack.NetStandard/WebApi/Chat/UpdateMessageRequest.cs
+++ Slack.NetStandard/WebApi/Chat/UpdateMessageRequest.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Newtonsoft.Json;
+using Slack.NetStandard.Messages;
 using Newtonsoft.Json.Converters;
 using Slack.NetStandard.Messages.Blocks;
 
@@ -28,10 +27,17 @@ namespace Slack.NetStandard.WebApi.Chat
         [JsonProperty("link_names", NullValueHandling = NullValueHandling.Ignore)]
         public bool? LinkNames { get; set; }
 
+        [JsonProperty("file_ids", NullValueHandling = NullValueHandling.Ignore)]
+        public string[] FileIds { get; set; }
+
+        [JsonProperty("attachments", NullValueHandling = NullValueHandling.Ignore)]
+        public IList<Attachment> Attachments { get; set; } = new List<Attachment>();
+
         [JsonProperty("parse", NullValueHandling = NullValueHandling.Ignore)]
         [JsonConverter(typeof(StringEnumConverter))]
         public MessageParsing MessageParsing { get; set; }
 
         public bool ShouldSerializeBlocks() => Blocks?.Any() ?? false;
+        public bool ShouldSerializeAttachments() => Attachments?.Any() ?? false;
     }
 }
