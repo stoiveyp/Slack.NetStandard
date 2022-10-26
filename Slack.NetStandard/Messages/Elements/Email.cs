6cafd3815f92db545b772cc00d91cdf3de00d428
Steven Pears <steven.pears@live.co.uk>
2022-10-26 08:36:22 +0100

    new input elements

---
+++ Slack.NetStandard/Messages/Elements/Email.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Slack.NetStandard.Messages.Elements;
+
+public class Email : IMessageElement
+{
+    public static string ElementType = "email_text_input";
+    public string Type => ElementType;
+
+    [JsonProperty("action_id",NullValueHandling = NullValueHandling.Ignore)]
+    public string ActionId { get; set; }
+
+    [JsonProperty("placeholder",NullValueHandling = NullValueHandling.Ignore)]
+    public PlainText Placeholder { get; set; }
+
+    [JsonProperty("initial_value",NullValueHandling = NullValueHandling.Ignore)]
+    public string InitialValue { get; set; }
+
+    [JsonProperty("dispatch_action_config",NullValueHandling = NullValueHandling.Ignore)]
+    public DispatchActionConfig DispatchActionConfig { get; set; }
+
+    [JsonProperty("focus_on_load",NullValueHandling = NullValueHandling.Ignore)]
+    public bool? FocusOnLoad { get; set; }
+
+}
