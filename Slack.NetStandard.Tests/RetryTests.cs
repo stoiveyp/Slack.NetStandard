4c710b0cdac8737f87abfc7ce090578454693d94
Steven Pears <steven.pears@live.co.uk>
2024-12-11 13:06:03 +0000

    Add RateLimited extension method for Slack API (#101)
    * Add retry logic for rate limited request
    
    * Fix logic to make sure retries doesn't include first attempted call
    
    * Add exception in case someone uses method incorrectly

---
+++ Slack.NetStandard.Tests/RetryTests.cs
@@ -0,0 +1,71 @@
+﻿using Slack.NetStandard.WebApi;
+using System.Net.Http;
+using Xunit;
+using System.Threading.Tasks;
+using System;
+
+namespace Slack.NetStandard.Tests
+{
+    public class RetryTests
+    {
+        [Fact]
+        public async Task RetryShortCircuitsOnSuccessfulResponse()
+        {
+            var response = new WebApiResponse();
+            response.OK = true;
+
+            var http = new HttpClient(new ActionHandler(req => Task.FromResult(response)));
+            var client = new SlackWebApiClient(http);
+            var called = 0;
+
+            await client.RateLimited(c => { called++; return Task.FromResult(response); }, 1);
+            Assert.Equal(1, called);
+        }
+
+        [Fact]
+        public async Task RetryShortCircuitsOnFailedResponseWithoutRetry()
+        {
+            var response = new WebApiResponse();
+            response.OK = false;
+
+            var http = new HttpClient(new ActionHandler(req => Task.FromResult(response)));
+            var client = new SlackWebApiClient(http);
+            var called = 0;
+
+            await client.RateLimited(c => { called++; return Task.FromResult(response); }, 1);
+            Assert.Equal(1, called);
+        }
+
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(3, 4)]
+        [InlineData(5, 6)]
+        public async Task RetryReturnsFailedResponseAfterMaxAttempts(int retries, int expected)
+        {
+            var response = new WebApiResponse();
+            response.OK = false;
+            response.RetryAfter = TimeSpan.FromSeconds(1);
+
+            var http = new HttpClient(new ActionHandler(req => Task.CompletedTask));
+            var client = new SlackWebApiClient(http);
+            var called = 0;
+
+            await TakesAtLeast(client.RateLimited(c => { called++; return Task.FromResult(response); }, retries),TimeSpan.FromSeconds(retries));
+            Assert.Equal(expected, called);
+        }
+
+        private async Task TakesAtLeast(Task method, TimeSpan minDuration)
+        {
+            var delay = Task.Delay(minDuration);
+            var result = await Task.WhenAny(method, delay);
+            if(result == method)
+            {
+                throw new InvalidOperationException("Method completed before minimum duration");
+            }
+            await method;
+        }
+
+
+
+    }
+}
---
+++ Slack.NetStandard/RetryExtension.cs
@@ -0,0 +1,39 @@
+﻿using Slack.NetStandard.WebApi;
+using System;
+using System.Threading.Tasks;
+
+namespace Slack.NetStandard
+{
+    public static class RetryExtension
+    {
+        public static async Task<T> RateLimited<T>(this ISlackApiClient api, Func<ISlackApiClient, Task<T>> action, int maxRetries = 3) where T:WebApiResponseBase
+        {
+            if(maxRetries == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxRetries), "maxRetries must be greater than 0");
+            }
+
+            var retries = 0;
+            var lastResponse = default(T);
+
+            while(retries < maxRetries)
+            {
+                if (lastResponse != null)
+                {
+                    await Task.Delay(lastResponse.RetryAfter.Value);
+                    retries++;
+                }
+
+                var response = await action(api);
+                if (response.OK || !response.RetryAfter.HasValue)
+                {
+                    return response;
+                }
+
+                lastResponse = response;
+            }
+
+            return lastResponse;
+        }
+    }
+}
