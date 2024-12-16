c64136b7b41e8416b7b2021c66aa529f094c0029
Steven Pears <steven.pears@live.co.uk>
2024-12-16 14:25:12 +0000

    tidy up retry method

--- Slack.NetStandard/RetryExtension.cs
+++ Slack.NetStandard/RetryExtension.cs
@@ -8,24 +8,28 @@ namespace Slack.NetStandard
     {
         public static async Task<T> RateLimited<T>(this ISlackApiClient api, Func<ISlackApiClient, Task<T>> action, int maxRetries = 3) where T:WebApiResponseBase
         {
+            static bool ShouldReturn(WebApiResponseBase response) => response.OK || !response.RetryAfter.HasValue;
+
             if(maxRetries == 0)
             {
                 throw new ArgumentOutOfRangeException(nameof(maxRetries), "maxRetries must be greater than 0");
             }
 
             var retries = 0;
-            var lastResponse = default(T);
+            var lastResponse = await action(api);
 
-            while(retries < maxRetries)
+            if(ShouldReturn(lastResponse))
             {
-                if (lastResponse != null)
-                {
-                    await Task.Delay(lastResponse.RetryAfter.Value);
-                    retries++;
-                }
+                return lastResponse;
+            }
 
+            while (retries < maxRetries)
+            {
+                await Task.Delay(lastResponse.RetryAfter.Value);
+
+                retries++;
                 var response = await action(api);
-                if (response.OK || !response.RetryAfter.HasValue)
+                if (ShouldReturn(response))
                 {
                     return response;
                 }
