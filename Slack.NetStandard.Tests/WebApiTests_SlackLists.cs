8331adae1a1c48717a615d960ced215f2b34275d
Steven Pears <steven.pears@live.co.uk>
2025-11-25 21:55:22 +0000

    separate slacklist items tests

--- Slack.NetStandard.Tests/WebApiTests_SlackLists.cs
+++ Slack.NetStandard.Tests/WebApiTests_SlackLists.cs
@@ -28,38 +28,5 @@ namespace Slack.NetStandard.Tests
                     Utility.CompareJson(jo, "Web_SlackListsUpdateRequest.json");
                 });
         }
-
-        [Fact]
-        public async Task Items_Info()
-        {
-            var fileId = "F1234567";
-            var itemId = "Rec014K005UQJ";
-            await Utility.AssertEncodedWebApi(c => c.SlackLists.Items.Info("F1234567", itemId, true), "slackLists.items.info",
-                "Web_SlackListsInfoResponse.json", nvc =>
-                {
-                    Assert.Equal(fileId, nvc["list_id"]);
-                    Assert.Equal(itemId, nvc["item_id"]);
-                    Assert.Equal(true.ToString(), nvc["include_is_subscribed"]);
-                });
-        }
-
-        [Fact]
-        public async Task Items_Create()
-        {
-            var fileId = "F1234567";
-            var itemId = "Rec014K005UQJ";
-            await Utility.AssertEncodedWebApi(c => c.SlackLists.Items.Create(new SlackListItemCreateRequest
-            {
-                ListId = fileId,
-                ParentItemId = itemId,
-                InitialFields = [new SelectCellDefinition("Col1000000",["OptHIGH123"])]
-            }), "slackLists.items.create",
-                "Web_SlackListsInfoResponse.json", nvc =>
-                {
-                    Assert.Equal(fileId, nvc["list_id"]);
-                    Assert.Equal(itemId, nvc["item_id"]);
-                    Assert.Equal(true.ToString(), nvc["include_is_subscribed"]);
-                });
-        }
     }
 }
---
+++ Slack.NetStandard.Tests/WebApiTests_SlackListsItems.cs
@@ -0,0 +1,43 @@
+﻿using Slack.NetStandard.WebApi.SlackLists;
+using Slack.NetStandard.WebApi.SlackLists.CellDefinition;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Slack.NetStandard.Tests
+{
+    public class WebApiTests_SlackListsItems
+    {
+        [Fact]
+        public async Task Info()
+        {
+            var fileId = "F1234567";
+            var itemId = "Rec014K005UQJ";
+            await Utility.AssertEncodedWebApi(c => c.SlackLists.Items.Info("F1234567", itemId, true), "slackLists.items.info",
+                "Web_SlackListsInfoResponse.json", nvc =>
+                {
+                    Assert.Equal(fileId, nvc["list_id"]);
+                    Assert.Equal(itemId, nvc["item_id"]);
+                    Assert.Equal(true.ToString(), nvc["include_is_subscribed"]);
+                });
+        }
+
+        [Fact]
+        public async Task Create()
+        {
+            var fileId = "F1234567";
+            var itemId = "Rec014K005UQJ";
+            await Utility.AssertEncodedWebApi(c => c.SlackLists.Items.Create(new SlackListItemCreateRequest
+            {
+                ListId = fileId,
+                ParentItemId = itemId,
+                InitialFields = [new SelectCellDefinition("Col1000000",["OptHIGH123"])]
+            }), "slackLists.items.create",
+                "Web_SlackListsInfoResponse.json", nvc =>
+                {
+                    Assert.Equal(fileId, nvc["list_id"]);
+                    Assert.Equal(itemId, nvc["item_id"]);
+                    Assert.Equal(true.ToString(), nvc["include_is_subscribed"]);
+                });
+        }
+    }
+}
